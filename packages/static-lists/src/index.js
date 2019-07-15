c984424079b54273eab83a650446417f9aeb8103
m.navarro <m.navarro@payvision.com>
2019-07-15 21:50:10 +0200

    feat: new static lists package

---
+++ packages/static-lists/src/index.js
@@ -0,0 +1,4 @@
+// const { bigQuery } = require('@i62navpm/cloud-sdk')
+// const normalList = require('../schemas/normalList')
+// const bilingualList = require('../schemas/bilingualList')
+// const voluntaryList = require('../schemas/voluntaryList')
