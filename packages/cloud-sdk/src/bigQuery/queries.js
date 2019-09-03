d3a55230110195db71bcf067b94a92ddb342e8c9
m.navarro <m.navarro@payvision.com>
2019-09-03 08:11:18 +0200

    feat: change get movements by date

--- packages/cloud-sdk/src/bigQuery/queries.js
+++ packages/cloud-sdk/src/bigQuery/queries.js
@@ -50,12 +50,13 @@ module.exports = bigquery => {
     const dateInitial = '2019-07-24'
 
     const query = `
-    WITH subQ1 as (SELECT orden, _TABLE_SUFFIX as tableId  FROM \`${datasetId}.*\`  WHERE apellidosynombre = @name and nif = @nif),
-         subQ2 as (SELECT nif, apellidosynombre, subQ1.tableId FROM subQ1, \`${datasetId}.*\` as datasetId WHERE datasetId.orden < subQ1.orden and datasetId._TABLE_SUFFIX = subQ1.tableId),
-         subQ3 as (SELECT tableId, struct(date as date, -count(DISTINCT subQ2.apellidosynombre) as counter) as data FROM subQ2, \`assignmentList.*\` as assignmentList WHERE assignmentList.nif = subQ2.nif and assignmentList.apellidosynombre = subQ2.apellidosynombre and assignmentList.date BETWEEN @date and CURRENT_DATE() GROUP BY tableId, date )
-
-    SELECT tableId, ARRAY_CONCAT_AGG([struct(@date as date, 0 as counter), data] order by data.date) as info FROM subQ3 GROUP BY tableId
-
+    WITH subQ1 as (select orden, _TABLE_SUFFIX as tableId  from \`${datasetId}.*\`  where apellidosynombre = @name and nif = @nif),
+         subQ2 as (select nif, apellidosynombre, subQ1.tableId from subQ1, \`${datasetId}.*\` as normalList where normalList.orden < subQ1.orden and normalList._TABLE_SUFFIX = subQ1.tableId),
+         subQ3 as (select tableId, date, -count(DISTINCT subQ2.apellidosynombre) as counter from subQ2, \`assignmentList.*\` as assignmentList where assignmentList.nif = subQ2.nif and assignmentList.apellidosynombre = subQ2.apellidosynombre and assignmentList.date BETWEEN @date and CURRENT_DATE() group by tableId, date ),
+         subQ4 as (select tableId, Date(@date) as date, count(subQ2.apellidosynombre) as counter from subQ2 group by tableId),
+         subQ5 as (select tableId, struct(date, counter, SUM(counter) OVER (PARTITION BY tableId ORDER BY date) as acc) as data from (select date, tableId, counter from subQ3 UNION ALL select date, tableId, counter from subQ4) as unionQuery)
+   
+    select tableId, ARRAY_CONCAT_AGG([data] order by data.date) as info from subQ5 group by tableId
     `
     const options = {
       query,
